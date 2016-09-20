2f1ff71d5816c9a2cd8894250f8a939c37d9f5cf
Stephanie Kwak <stephanieyoungkwak@gmail.com>
2016-09-20 12:28:42 -0700

    Add zlib module, construct new buffer from base64 string

--- lambda-spike/app.js
+++ lambda-spike/app.js
@@ -8,10 +8,20 @@ process.env['LD_LIBRARY_PATH'] = process.env['LAMBDA_TASK_ROOT'] + '/bin';
 const pm = require('pdf-merge');
 const request = require('request');
 const async = require('async');
+const zlib = require('zlib');
 
 exports.handler = function(event, context) {
-  const data = event.data;
-  console.log(data);
+  const base64Data = event.data;
+  console.log(base64Data);
+
+  const buffer = Buffer.from(base64Data, 'base64');
+  zlib.unzip(buffer, (err, buffer) => {
+    if (!err) {
+      console.log(buffer.toString());
+    } else {
+    // handle error
+    }
+  });
 
 
 }
