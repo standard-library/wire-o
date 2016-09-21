a089809c32397e133b0484da90832de1ff608ada
Stephanie Kwak <stephanieyoungkwak@gmail.com>
2016-09-21 08:48:59 -0700

    Targz each pdf instead, move them into S3

--- lambda-spike/app.js
+++ lambda-spike/app.js
@@ -5,22 +5,51 @@ const exec = require('child_process').exec;
 process.env['PATH'] = process.env['PATH'] + ':' + process.env['LAMBDA_TASK_ROOT'] + '/bin';
 process.env['LD_LIBRARY_PATH'] = process.env['LAMBDA_TASK_ROOT'] + '/bin';
 
-const pm = require('pdf-merge');
+const pdfMerge = require('pdf-merge');
 const request = require('request');
 const async = require('async');
 const zlib = require('zlib');
-const tar = require('tar');
+const fs = require('fs');
+const AWS = require('aws-sdk');
+const s3 = new AWS.S3();
+const uuid = require('node-uuid');
+// const targz = require('tar.gz');
+// const tar = require('tar');
+// const unzip = require('unzip');
 
 exports.handler = function(event, context) {
+
   const base64Data = event.data;
-  let buff = new Buffer(base64Data, 'base64')
-  zlib.unzip(buff, (err, buffer) => {
-    if (!err) {
-      console.log(buffer.toString());
-    } else {
-    // handle error
-    }
+
+  async.each(base64Data, function (pdfData, callback) {
+    let buff = new Buffer(pdfData, 'base64');
+    zlib.unzip(buff, function (err, buffed) {
+      if (!err) {
+        const params = { Bucket: 'superglue', Key: uuid.v4() + ".pdf", Body: buffed };
+        s3.putObject(params, function (err, s3Data) {
+          if (err) {
+            console.log(err);
+          } else {
+            console.log('s3 data: ' + s3Data)
+            console.log('Successfully uploaded.');
+          }
+        });
+      } else {
+        console.log('error');
+      }
+    });
   });
 
+  var PDFMerge = require('pdf-merge');
+  var pdftkPath = './bin/pdftk'
+  // var pdftkPath = 'C:\\PDFtk\\bin\\pdftk.exe';
+  // var pdfFiles = [__dirname + '/pdf1.pdf', __dirname + '/pdf2.pdf'];
+  // var pdfMerge = new PDFMerge(pdfFiles, pdftkPath);
+
+  // pdfMerge
+  //   .asBuffer()
+  //   .merge(function(error, buffer) {
+  //fs.writeFileSync(__dirname + '/merged.pdf', buffer);
+});
 
 }
--- lambda-spike/send_pdf.sh
+++ lambda-spike/send_pdf.sh
@@ -1,10 +1,6 @@
-# zip any pdfs in the pdf folder
-rm ./pdf/pdfs.tar.gz
+tar cvzf ./pdf/first_pdf.tar.gz ./pdf/PCAH_PDF_TEMPLATE.pdf
+tar cvzf ./pdf/second_pdf.tar.gz ./pdf/hello.pdf
 
-# compress pdfs into a gzipped tarball
-tar cvzf ./pdf/pdfs.tar.gz ./pdf/*.pdf
-
-# convert tar.gz into base64 string
-tar_gzipped_pdfs=$( base64 ./pdf/pdfs.tar.gz)
-#
-curl -X POST -d "{\"data\":\"$tar_gzipped_pdfs\"}" https://o64722rmyh.execute-api.us-east-1.amazonaws.com/beta
+first_pdf_targz=$( base64 ./pdf/first_pdf.tar.gz)
+second_pdf_targz=$( base64 ./pdf/second_pdf.tar.gz)
+curl -X POST -d "{\"data\": [\"$first_pdf_targz\", \"$second_pdf_targz\"]}" https://o64722rmyh.execute-api.us-east-1.amazonaws.com/beta
