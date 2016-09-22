63efb8f33b9a25b0af924b9e428d562843caf453
Jack Jennings <jack@remote.gd>
2016-09-22 12:18:35 -0700

    Remove php view rendering layer

--- src/settings.php
+++ src/settings.php
@@ -4,11 +4,6 @@ return [
         'displayErrorDetails' => true, // set to false in production
         'addContentLengthHeader' => false, // Allow the web server to send the content-length header
 
-        // Renderer settings
-        'renderer' => [
-            'template_path' => __DIR__ . '/../templates/',
-        ],
-
         // Monolog settings
         'logger' => [
             'name' => 'slim-app',
