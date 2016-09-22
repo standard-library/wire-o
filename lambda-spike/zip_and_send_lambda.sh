457ed7b531a1e170934a7b069dea69aa92269abf
Stephanie Kwak <stephanieyoungkwak@gmail.com>
2016-09-22 13:28:55 -0700

    Add .gitignore, add package install to script

--- lambda-spike/zip_and_send_lambda.sh
+++ lambda-spike/zip_and_send_lambda.sh
@@ -1,3 +1,4 @@
+npm install
 rm app.zip
 zip -r app.zip app.js bin node_modules
 aws lambda update-function-code --region us-east-1 --function-name arn:aws:lambda:us-east-1:397120925662:function:mergePdfs --zip-file fileb://app.zip
