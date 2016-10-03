059bcd0a732e2f1bf3599f8823b128b9009fdedd
Stephanie Kwak <stephanieyoungkwak@gmail.com>
2016-10-03 12:46:01 -0700

    Start modularizing save PDFs to lambda server function

--- lambda-spike/app.js
+++ lambda-spike/app.js
@@ -5,46 +5,49 @@ var exec = require('child_process').exec;
 process.env['PATH'] = process.env['PATH'] + ':' + process.env['LAMBDA_TASK_ROOT'] + '/bin';
 process.env['LD_LIBRARY_PATH'] = process.env['LAMBDA_TASK_ROOT'] + '/bin';
 
-exports.handler = function(event, context) {
-  var PDFMerge = require('pdf-merge');
-  var request = require('request');
-  var async = require('async');
-  var zlib = require('zlib');
-  var fs = require('fs');
-  var AWS = require('aws-sdk');
-  var s3 = new AWS.S3();
-  var uuid = require('node-uuid');
-  var urls = event.pdfUrls;
+var PDFMerge = require('pdf-merge');
+var request = require('request');
+var async = require('async');
+var fs = require('fs');
+var AWS = require('aws-sdk');
+var s3 = new AWS.S3();
+var uuid = require('node-uuid');
 
+
+exports.handler = function(event, context) {
   console.time('lambda runtime');
-  async.each(urls, function(pdfUrl, callback) {
-    var requestSettings = {
+
+  var PdfSaver = function(urls, cb) {
+    console.time('save pdfs to tmp directory');
+    async.each(urls, function(pdfUrl, callback) {
+      var requestSettings = {
         method: 'GET',
         url: pdfUrl,
         encoding: null
-     };
+      };
 
-    request(requestSettings, function(err, response, buffer) {
-      var base64Pdf = new Buffer(buffer).toString('base64');
-      if (!err) {
-        writeToTmp(base64Pdf);
-        console.log('File encoded.');
-        callback();
+      request(requestSettings, function(err, response, buffer) {
+        var base64Pdf = new Buffer(buffer).toString('base64');
+        if (!err) {
+          writeToTmp(base64Pdf);
+          console.log('File encoded.');
+          callback();
+        } else {
+          callback('Error encoding file.');
+        }
+      });
+    }, function(err) {
+      if (err) {
+        console.log('File did not process');
       } else {
-        callback('Error encoding file.');
+        console.log('All files processed');
+        console.timeEnd('save pdfs to tmp directory');
+        cb();
       }
     });
-  }, function(err) {
-    if (err) {
-      console.log('File did not process');
-    } else {
-      console.log('All files processed');
-      findTmpFiles();
-    }
-  });
+  }
 
   function writeToTmp(base64) {
-    fs.unlink('/tmp/', function(err) { console.log('tmp files deleted') });
     var filePath = '/tmp/' + uuid.v4() + '.pdf'
 
     fs.writeFile(filePath, new Buffer(base64, "base64"), function (err) {
@@ -100,4 +103,6 @@ exports.handler = function(event, context) {
         });
     });
   }
+
+  PdfSaver(event.pdfUrls, findTmpFiles);
 }
