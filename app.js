eface92a0b691b325d3b9d9c71daefe890048602
Jack Jennings <jack@remote.gd>
2016-10-21 09:04:43 -0700

    Shim in promises to callback-style functions using Promise.promisify

--- app.js
+++ app.js
@@ -3,9 +3,6 @@
 process.env['PATH'] = process.env['PATH'] + ':' + process.env['LAMBDA_TASK_ROOT'] + '/bin';
 process.env['LD_LIBRARY_PATH'] = process.env['LAMBDA_TASK_ROOT'] + '/bin';
 
-var async = require('async');
-var Promise = require('bluebird');
-
 var downloadPdfs = require('./lib/downloadPdfs');
 var mergePdfs = require('./lib/mergePdfs');
 var uploadPdf = require('./lib/uploadPdf');
@@ -14,20 +11,12 @@ exports.handler = function(event, context) {
   console.time('Lambda runtime');
 
   downloadPdfs(event.pdfUrls)
-    .then(function (filePaths) {
-      async.waterfall([
-        function getFilePaths(callback) {
-          callback(null, filePaths);
-        },
-        mergePdfs,
-        uploadPdf
-      ], function (error, result) {
-        if (error) {
-          console.log(error);
-        } else {
-          console.timeEnd('Lambda runtime');
-          context.succeed(result);
-        }
-      });
+    .then(mergePdfs)
+    .then(uploadPdf)
+    .then(function (url) {
+      context.succeed(url);
+      console.timeEnd('Lambda runtime');
+    }).catch(function (error) {
+      console.log(error);
     });
 }
