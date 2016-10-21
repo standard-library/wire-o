5a332d0721d1d1b337a97ab1709253f2319af307
Jack Jennings <jack@remote.gd>
2016-10-20 22:50:40 -0700

    Cleanup requires

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 'use strict';
 
-var exec = require('child_process').exec;
-
 process.env['PATH'] = process.env['PATH'] + ':' + process.env['LAMBDA_TASK_ROOT'] + '/bin';
 process.env['LD_LIBRARY_PATH'] = process.env['LAMBDA_TASK_ROOT'] + '/bin';
 
