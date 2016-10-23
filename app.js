ba6f4ecb5c4adc24a56a5ac35dcba03fec869280
Jack Jennings <jack@remote.gd>
2016-10-22 23:44:01 -0700

    Rename uploadPDF to storePDF to be more generic

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ process.env['LD_LIBRARY_PATH'] = process.env['LAMBDA_TASK_ROOT'] + '/bin';
 var downloadPdfs = require('./lib/downloadPdfs');
 var mergePdfs = require('./lib/mergePdfs');
 var uploadToS3 = require('./lib/uploadToS3')({ bucket: 'superglue' });
-var uploadPdf = require('./lib/uploadPdf')({ uploader: uploadToS3 });
+var storePDF = require('./lib/storePdf')({ storage: uploadToS3 });
 var formatResponse = require('./lib/formatResponse');
 
 exports.handler = function(event, context, callback) {
@@ -14,7 +14,7 @@ exports.handler = function(event, context, callback) {
 
   downloadPdfs(event.pdfUrls)
     .then(mergePdfs)
-    .then(uploadPdf)
+    .then(storePDF)
     .then(formatResponse)
     .then(callback)
     .then(function () {
