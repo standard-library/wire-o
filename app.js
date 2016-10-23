589555325dd5998c4b46d0275a4a4b9a4fb2a4fe
Jack Jennings <jack@remote.gd>
2016-10-22 23:00:41 -0700

    Move S3 logic into a separate uploader function

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ process.env['LD_LIBRARY_PATH'] = process.env['LAMBDA_TASK_ROOT'] + '/bin';
 
 var downloadPdfs = require('./lib/downloadPdfs');
 var mergePdfs = require('./lib/mergePdfs');
-var uploadPdf = require('./lib/uploadPdf')({ bucket: 'superglue' });
+var uploadToS3 = require('./lib/uploadToS3')({ bucket: 'superglue' });
+var uploadPdf = require('./lib/uploadPdf')({ uploader: uploadToS3 });
 
 exports.handler = function(event, context) {
   console.time('Lambda runtime');
