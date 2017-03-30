f9295fe54311127c5dffc30dcb3b354a34d48217
Stephanie Kwak <stephanieyoungkwak@gmail.com>
2017-03-30 13:53:42 -0700

    Add space #5

--- test/uploadToS3.test.js
+++ test/uploadToS3.test.js
@@ -8,7 +8,7 @@ import uploadToS3 from '../lib/uploadToS3';
 
 describe('uploadToS3 module', function () {
   it('returns function', function (done) {
-    expect(uploadToS3({ bucket: 'foo-bar'})).to.be.an.instanceof(Function);
+    expect(uploadToS3({ bucket: 'foo-bar' })).to.be.an.instanceof(Function);
     done();
   });
 
