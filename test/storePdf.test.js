d0a02122de7441f1a4f98ee652d1fa6587ce3361
Stephanie Kwak <stephanieyoungkwak@gmail.com>
2017-03-30 13:55:35 -0700

    Change argument to storePdf #5

--- test/storePdf.test.js
+++ test/storePdf.test.js
@@ -10,7 +10,7 @@ import uploadToS3 from '../lib/uploadToS3';
 describe('storePdf module', function () {
   it('returns function', function (done) {
     const storage = uploadToS3({ bucket: 'foo-bar'});
-    expect(storePdf(storage)).be.an.instanceOf(Function);
+    expect(storePdf({ storage })).be.an.instanceOf(Function);
     done();
   });
 });
