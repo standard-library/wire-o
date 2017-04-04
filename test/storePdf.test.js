a811f7b677a056632f3b76267177da350f4b79ef
Jack Jennings <jack@standard-library.com>
2017-04-03 23:37:49 -0700

    Change casing of storePDF function name for consistency. #6

--- test/storePdf.test.js
+++ test/storePdf.test.js
@@ -6,19 +6,19 @@ import fs from 'fs';
 import sinon from 'sinon';
 import sinonChai from 'sinon-chai';
 
-import storePdf from '../lib/storePdf';
+import storePDF from '../lib/storePdf';
 
 chai.use(sinonChai);
 
-describe('storePdf module', function () {
+describe('storePDF module', function () {
   it('returns function', function () {
     const storage = () => {};
-    expect(storePdf({ storage })).be.an.instanceOf(Function);
+    expect(storePDF({ storage })).be.an.instanceOf(Function);
   });
 
   it('calls the returned function and returns the PDF URL', function (done) {
     const storage = sinon.spy();
-    const store = storePdf({ storage });
+    const store = storePDF({ storage });
     const buffer = fs.readFileSync(`${__dirname}/fixtures/hello.pdf`);
 
     store({ buffer }).then(function (response) {
